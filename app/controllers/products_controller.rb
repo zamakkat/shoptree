bb2552d3a2087dcd96d2f9cfcf1ff927b07bb606
Ho Tuan Duong <zamakkat@gmail.com>
2016-06-30 23:49:33 +0800

    Allow ProductsController to receive JS requests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,9 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    respond_to do |format|
+      format.html
+      format.js { render status: :ok }
+    end
   end
 end
