5dacc3f73f3817c89ac1597e915f20acd547d7a7
Ho Tuan Duong <zamakkat@gmail.com>
2016-07-01 00:00:08 +0800

    Add ProductsController spec for index

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,12 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
+  describe 'GET #index' do
+    context 'html' do
+      def do_request
+        get :index
+      end
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+      before { do_request }
+
+      it { expect(assigns[:products]).to be_a(Array) }
+      it { expect(response).to be_success }
+    end
+
+    context 'js' do
+      def do_request
+        xhr :get, :index
+      end
+
+      before { do_request }
+
+      it { expect(assigns[:products]).to be_a(Array) }
+      it { expect(response).to have_http_status(200) }
+      it { expect(response).to render_template('products/index') }
     end
   end
-
 end
