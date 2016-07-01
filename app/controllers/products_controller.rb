49883b2ba084fd7f48cdb5db9af6c63decfa1a53
Ho Tuan Duong <zamakkat@gmail.com>
2016-07-01 18:05:27 +0800

    Hide empty categories. Add filtering of categories to controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @category = Category.find_by(id: params[:category_id])
+    if @category
+      @products = Product.where(category_id: @category.self_and_descendant_ids)
+    else
+      @products = Product.all
+    end
     respond_to do |format|
       format.html do
         @root_categories = Category.roots
