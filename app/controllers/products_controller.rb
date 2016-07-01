ea4f92358bab1b5de8e8a4715ca4755672c1fc4f
Ho Tuan Duong <zamakkat@gmail.com>
2016-07-01 20:50:46 +0800

    Fix products controller after removing category_id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   def index
     @category = Category.find_by(id: params[:category_id])
     if @category
-      @products = Product.joins(:category_products).where(category_id: @category.self_and_descendant_ids).uniq
+      @products = Product.joins(:category_products).where('category_products.category_id' => @category.self_and_descendant_ids).uniq
     else
       @products = Product.all.uniq
     end
