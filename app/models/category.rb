8ef0987413506c7de68c92f947c73545da02c84a
Ho Tuan Duong <zamakkat@gmail.com>
2016-07-03 00:58:38 +0800

    Add the rest of category views

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-  has_many :category_products
+  has_many :category_products, dependent: :destroy
   has_many :products, through: :category_products
 
   has_closure_tree
