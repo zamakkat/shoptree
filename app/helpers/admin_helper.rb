982bb3d9b09f14a3b27a380df3383d01bd5dd5fc
Ho Tuan Duong <zamakkat@gmail.com>
2016-07-03 14:06:41 +0800

    Add link to categories

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,5 +1,8 @@
 module AdminHelper
   def category_hierarchy(category)
-    ([category] + category.ancestors).reverse.collect(&:name).join(' > ')
+    ([category] + category.ancestors).reverse
+      .collect { |c| link_to(c.name, admin_category_path(c)) }
+      .join(' > ')
+      .html_safe
   end
 end
