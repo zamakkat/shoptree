01af51e15f758faa81c3bf656c8bdc7a30b8d01a
Ho Tuan Duong <zamakkat@gmail.com>
2016-07-03 11:50:39 +0800

    Add more model specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   context 'associations' do
-    it { is_expected.to have_many(:category_products) }
+    it { is_expected.to have_many(:category_products).dependent(:destroy) }
     it { is_expected.to have_many(:categories) }
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
   end
 end
